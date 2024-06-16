78ae3b9cc131ef5c70cc4fd4931d9cf4d8103dfc
Grace Lee <jaeeungracelee@gmail.com>
2024-06-16 15:50:32 -0700

    first draft - added components, toggles, and responsive elements

---
+++ src/components/storyboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Storyboard = () => {
+  return (
+    <div>
+      <h1>my storyboard</h1>
+      <p>this is the storyboard page.</p>
+    </div>
+  );
+};
+
+export default Storyboard;
