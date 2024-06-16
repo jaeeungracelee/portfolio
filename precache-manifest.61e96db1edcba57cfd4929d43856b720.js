9c4cf517db6906f57fb78702337c9bd3fb64228a
Grace Lee <jaeeungracelee@gmail.com>
2024-06-16 15:55:45 -0700

    Updates

---
+++ precache-manifest.61e96db1edcba57cfd4929d43856b720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7e802323f382b4813440dfb0e7bc79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7520884f61d53a92f5e",
+    "url": "/static/css/main.5901cabe.chunk.css"
+  },
+  {
+    "revision": "9ebc332af79379070bff",
+    "url": "/static/js/2.e1526bcb.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "f7520884f61d53a92f5e",
+    "url": "/static/js/main.214f2cd8.chunk.js"
+  },
+  {
+    "revision": "38b441cba92b9c74f03f",
+    "url": "/static/js/runtime~main.16e34d5d.js"
+  }
+]);
